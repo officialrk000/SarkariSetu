4335b2837dbf4cd91b75be712b8aedec6b64abd7
officialrk000 <officialrk000@gmail.com>
2026-07-05 16:53:07 +0000

    commit

--- src/lib/config.ts
+++
@@ -1,2 +0,0 @@
-// Paste your Google Apps Script Web App URL here
-export const APPS_SCRIPT_URL = "https://script.google.com/macros/s/AKfycbzmy_kjTZjsjYaif7xT405HLCOhtgMm4PrOJTBWwekhM4NjX1hvguHB7EyogL97QWCR/exec";
